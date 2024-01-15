73ae3e016c30d3646cecbc9b77487cfff397d569
kelseykopper <kelseyannkopper@gmail.com>
2024-01-15 17:19:52 -0600

    read map function
    updates to read_map() so that the way a cipher_map is constructed depends on file mode

--- cipher.py
+++ cipher.py
@@ -5,12 +5,16 @@ def read_map(map_name, mode):
   """ Reads cipher map from the given text file. """
   cipher_map = dict()
   map = open(map_name, "r", encoding="utf-8")
-
-  for line in map: 
-    # split each line into a list of 2 items (key and value)
-    words = line.split()
-    # insert into the map. assumes no duplicates
-    cipher_map[words[0]] = words[1]
+  if mode == "-en": 
+    for line in map: 
+      # split each line into a list of 2 items (key and value)
+      words = line.split()
+      # insert into the map. assumes no duplicates
+      cipher_map[words[0]] = words[1]
+  else: 
+    for line in map:
+      words = line.split()
+      cipher_map[words[1]] = words[0]
 
   map.close()
   return cipher_map
@@ -19,16 +23,25 @@ def gen_cipher(start, end, rot):
   """ Generate a cipher map. """
   cipher_map = dict()
 
+  # TODO: generate a substitution cipher
+
   return cipher_map
 
 def code_file(src_name, dst_name, map, mode): 
-  """ Encodes/decodes source file to destination file using the cipher map. """
+  """ Encodes/decodes source file to destination file using the cipher map.
+      Note: map must be constructed such that keys are found in the source file
+       and values are the "result" of encryption/decryption.
+  """
   src = open(src_name, "r", encoding="utf-8")
   dst = open(dst_name, "w", encoding="utf-8")
 
-  # for line in src: 
-  #   for letter in line: 
-  #     print(letter, end='\n')
+  for line in src: 
+    for letter in line: 
+        val = map.get(letter)
+        if (val != None):
+          dst.write(val)
+        else: 
+          dst.write(letter)
 
   src.close()
   dst.close()
@@ -37,7 +50,7 @@ if __name__ == '__main__':
   if len(sys.argv) < 5: 
      print(f"Usage: {sys.argv[0]} src.txt dst.txt <mode> -rot x")
 
-     print("Cipher may be specified by indicating the number of rotations. For caesar cipher, use -rot 3")
+     print("Cipher may be specified by indicating the number of rotations. For Caesar cipher, use -rot 3")
 
      print("\nModes are as follows:")
      print("\t-en -> encode source file to destination")
@@ -52,23 +65,24 @@ if __name__ == '__main__':
   mode = sys.argv[3]
 
   # error checking
-  if mode != "-en" or mode != "-de":
+  if mode != "-en" and mode != "-de":
     print("Unknown mode.")
 
     print("\nModes are as follows:")
     print("\t-en -> encode source file to destination")
     print("\t-de -> decode source file to destination")
+    sys.exit()
 
   # obtaining cipher map
   if len(sys.argv) == 6: 
-    # need to generate a cipher map
+    # need to generate a cipher map, rotation has been provided
     rot = sys.argv[5]
     cipher_map = gen_cipher('A', 'Z', rot)
     
   elif len(sys.argv) == 5: 
-    # map file was provided
+    # map file was provided, read file
     map_name = sys.argv[4]
     cipher_map = read_map(map_name, mode)
 
-  code_file(src_name, dst_name, cipher_map, mode);
+  code_file(src_name, dst_name, cipher_map, mode)
 
