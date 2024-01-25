44f6023229e26c3339a3a87417018a1e3efd80ba
kelseykopper <kelseyannkopper@gmail.com>
2024-01-24 21:58:32 -0600

    created functions to generate a substitution cipher
    created functions to generate cipher map structures given a specific rotation and encode/decode mode

--- cipher.py
+++ cipher.py
@@ -1,5 +1,6 @@
 # @author Kelsey Kopper
 import sys
+import string
 
 def read_map(map_name, mode): 
   """ Reads cipher map from the given text file. """
@@ -10,8 +11,8 @@ def read_map(map_name, mode):
       # split each line into a list of 2 items (key and value)
       words = line.split()
       # insert into the map. assumes no duplicates
-      cipher_map[words[0]] = words[1]
-  else: 
+      cipher_map[words[0]] = words[1] 
+  elif mode == "-de": 
     for line in map:
       words = line.split()
       cipher_map[words[1]] = words[0]
@@ -19,11 +20,37 @@ def read_map(map_name, mode):
   map.close()
   return cipher_map
 
-def gen_cipher(start, end, rot):
+def gen_alphabet(alphabet, rot, map, mode):
+  """ Helper function to create a cipher structure for the given rotation. """
+
+  # get the last letter in keys alphabet before cipher values should wrap around
+  # to the start of the alphabet
+  last_letter = chr(ord(alphabet[-1]) - rot + 1)
+
+  for letter in alphabet:
+    if letter == last_letter:
+      # wrap back to start of alphabet
+      value = alphabet[0]
+    elif letter > last_letter: 
+      i = alphabet.index(value) # get the index of "value" from last iteration
+      value = alphabet[i + 1]  # increments value alphabetically
+    else: 
+      # convert letter to ASCII, add rotation, then convert back to char
+      value = chr(ord(letter) + rot)
+
+    if mode == "-en":
+      map[letter] = value
+    elif mode == "-de":
+      map[value] = letter
+
+def gen_cipher(rot, mode):
   """ Generate a cipher map. """
   cipher_map = dict()
+  lowercase_alphabet = list(string.ascii_lowercase)
+  uppercase_alphabet = list(string.ascii_uppercase)
 
-  # TODO: generate a substitution cipher
+  gen_alphabet(lowercase_alphabet, rot, cipher_map, mode)
+  gen_alphabet(uppercase_alphabet, rot, cipher_map, mode)
 
   return cipher_map
 
@@ -47,8 +74,14 @@ def code_file(src_name, dst_name, map, mode):
   dst.close()
 
 if __name__ == '__main__':
+  # testing mode
+  if len(sys.argv) == 2 and sys.argv[1] == "-test":
+    print("Testing functions ---------------------------------------------")
+    gen_cipher(3)
+    sys.exit()
+
   if len(sys.argv) < 5: 
-     print(f"Usage: {sys.argv[0]} src.txt dst.txt <mode> -rot x")
+     print(f"Usage: python3 {sys.argv[0]} src.txt dst.txt <mode> -rot x")
 
      print("Cipher may be specified by indicating the number of rotations. For Caesar cipher, use -rot 3")
 
@@ -64,7 +97,7 @@ if __name__ == '__main__':
   dst_name = sys.argv[2]
   mode = sys.argv[3]
 
-  # error checking
+  # error handling
   if mode != "-en" and mode != "-de":
     print("Unknown mode.")
 
@@ -76,8 +109,8 @@ if __name__ == '__main__':
   # obtaining cipher map
   if len(sys.argv) == 6: 
     # need to generate a cipher map, rotation has been provided
-    rot = sys.argv[5]
-    cipher_map = gen_cipher('A', 'Z', rot)
+    rot = int(sys.argv[5])
+    cipher_map = gen_cipher(rot, mode)
     
   elif len(sys.argv) == 5: 
     # map file was provided, read file
